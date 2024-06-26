476c7d97e780b5e2ca486f7a2953f9eabd5bdb7f
Ethan Ryan <144191914+JungitRyan@users.noreply.github.com>
2024-06-26 14:51:44 -0400

    Create operate function & assign variable values

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Variable initalizations
-let num1;
-let num2;
-let operator;
+let num1 = null;
+let num2 = null;
+let operator = null;
 
 // Math functions
 function add(num1, num2) {
@@ -18,4 +18,21 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   return num1 * num2;
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num2);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+  }
 }
