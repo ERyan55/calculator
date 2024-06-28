dfbff7ab2ea735acca4e3182fdf9ed4991679d9d
Ethan Ryan <144191914+JungitRyan@users.noreply.github.com>
2024-06-28 13:26:49 -0400

    Create helper functions

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Variable initalizations
-let num1 = null;
-let num2 = null;
+let firstNum = null;
+let secondNum = null;
 let operator = null;
+let displayVal = "";
 
 // Math functions
 function add(num1, num2) {
@@ -35,4 +36,36 @@ function operate(num1, num2, operator) {
       divide(num1, num2);
       break;
   }
+}
+
+// Decides whether to operate if both numbers are given a value, assigns a number to a value if not
+function operatorClick(num, op) {
+  assignOperator(op);
+  if (firstNum != null && secondNum != null) {
+    operate(firstNum, secondNum, operator);
+  } else {
+    assignNum(num);
+  }
+}
+
+// Assigns the operator
+function assignOperator(string) {
+  operator = string;
+}
+
+// Decides which number to assign the current display value to
+function assignNum(num) {
+  if (firstNum === null) {
+    firstNum = parseFloat(displayVal);
+  } else {
+    secondNum = parseFloat(displayVal);
+  }
+  displayVal = "";
+}
+
+// Displays the current value that has been entered
+function displayNum(numString) {
+  displayVal = displayVal.concat(numString);
+  const display = document.querySelector(".display");
+  display.textContent = displayVal;
 }
